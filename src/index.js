06b8b8daf8e53efdd08500e38933b2371fe34d6f
GwharshadSalimi <gawharshadsalimi3@gmail.com>
2025-08-09 10:46:08 +0430

    some changes in js was aded

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function handleAL(event) {
+  event.preventDefault();
 
+  new Typewriter("#pomee", {
+    strings: "دنیا همه هیچ . اهل دنیا همه هیچ",
+    autoStart: true,
+    delay: 1,
+  });
+}
+
+let submitElement = document.querySelector("#submit-1");
+submitElement.addEventListener("click", handleAL);
